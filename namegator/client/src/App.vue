11a0cbe831aa89579843130b6dd22ebda66e28b2
Erasmo Sena <erasmo.sena@gmail.com>
2020-08-24 14:04:58 -0300

    instrodução ao graphql

---
+++ namegator/client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js</h6>
+    </div>
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import DomainList from "./components/DomainList";
+
+export default {
+	name: "App",
+	components: {
+		DomainList
+	}
+};
+</script>
+
+<style>
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
---
+++ namegator/client/src/components/DomainList.vue
@@ -0,0 +1,125 @@
+<template>
+  <div >
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">            
+            <AppItemList v-bind:items="prefixes" title="Prefixos" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>            
+          </div>
+          <div class="col-md">
+            <AppItemList v-bind:items="sufixes" title="Sufixos" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>            
+          </div>
+        </div>
+        <br />
+        <h5>
+          Domínios
+          <span class="badge badge-info">{{domains.length}}</span>
+        </h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                <div class="row">
+                  <div class="col-md">{{domain.name}}</div>
+                  <div class="col-md text-right">
+                    
+                    <a v-bind:href="domain.checkout" target="_blank" class="btn btn-info">
+                    <span class="fa fa-shopping-cart"></span>
+                    </a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+import AppItemList from "./AppItemList";
+import axios from "axios/dist/axios";
+
+export default {
+	name: "DomainList",
+	components: {
+		AppItemList
+	},
+	data: function() {
+		return {
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"]
+		};
+	},
+	methods: {
+		addPrefix(prefix) {
+			this.prefixes.push(prefix);
+			this.prefix = "";
+		},
+		addSufix(sufix) {
+			this.sufixes.push(sufix);
+			this.sufix = "";
+		},
+		deletePrefix(prefix) {
+			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+		},
+		deleteSufix(sufix) {
+			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+		}
+	},
+	computed: {
+		domains() {
+			console.log("generating");
+			const domains = [];
+			for (const prefix of this.prefixes) {
+				for (const sufix of this.sufixes) {
+					const name = prefix + sufix;
+					const url = name.toLowerCase();
+					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+					domains.push(
+						{
+							name,
+							checkout
+						}
+					);
+				}
+			}
+			return domains;
+		}
+	},
+	created(){
+		axios({
+			url:"http://localhost:4000",
+			method:"post",
+			data:{
+				query: `
+					{
+						prefixes: items( type: "prefix") {
+							id
+							type
+							description
+						}
+						sufixes: items( type: "sufix") {
+							id
+							type
+							description
+						}
+					}
+				`
+			}
+		}).then((response)=> {
+			const query = response.data;
+			this.prefixes = query.data.prefixes.map(it=>it.description);
+			this.sufixes = query.data.sufixes.map(it=>it.description);
+			console.log(query);
+		});
+	}
+};
+</script>
+
+<style>
+
+</style>
